bd886cb8f43191d601739fc4e43b1a0e5dd4939c
Akshay Rao <akshayrao@Akshays-Laptop.local>
2025-04-15 21:52:35 +0530

    data ingestion completed

--- Project-1/Readme.md
+++ Project-1/Readme.md
@@ -1,4 +1,11 @@
 Project - 1 (Hotel Reservation Prediction)
 
-Platform and resources :- AWS's S3 bucket
-tools:- python, pandas, jypter Notebook, Flask, numpy
+- Platform and resources :- AWS's S3 bucket
+- create a virtual env through conda.
+- tools:- python, pandas, jypter Notebook, Flask, numpy
+
+## Terraform 
+- through terraform create private bucket and role called mlops.
+- this mlops role has to permission to do operation on this data.
+- attache a bucket policy so that this role only can access to this bucket.
+- this is done so that the unauthorized person cannot acces the data in s3.
--- Project-1/src/data-ingestion.py
+++ Project-1/src/data-ingestion.py
@@ -1,5 +1,42 @@
 import boto3
+import botocore
 import pandas as pd
 import io
 
+# Make sure that the boto3 and botocore are in same version
+print("boto3 version:", boto3.__version__)
+print("botocore version:", botocore.__version__)
+
+
+# Assume mlops role
 sts_client = boto3.client('sts')
+
+assumed_role = sts_client.assume_role(
+    RoleArn= "arn:aws:iam::<aws_account_id>:role/mlops",
+    RoleSessionName="S3ReadAccess"
+)
+
+# Get credentials
+credentials = assumed_role['Credentials']
+
+# Set the creentials for s3 client to fetch the object/file
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=credentials['AccessKeyId'],
+    aws_secret_access_key= credentials['SecretAccessKey'],
+    aws_session_token=credentials['SessionToken']
+)
+
+# Setup bucket name, file path in bucket and the local path to store
+bucket_name = "<bucket_name>"
+file_key = "<put file path>"
+local_file_path = "<path to store dataset>"
+
+# Fetch the file
+response = s3.download_file(bucket_name, file_key, local_file_path)
+
+# Use pandas to read the CSV content
+df = pd.read_csv(local_file_path)
+
+print(df.head())
+
