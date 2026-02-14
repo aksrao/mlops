2e39cd5b1faaaa77a5f1c02dbe41b9140ee8c37d
Akshay Rao <aksrao1998@gmail.com>
2026-02-14 16:55:15 +0530

    added get post actions

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+class Post(BaseModel):
+    firstName: str
+    lastName: str
+    age: int
+    married: bool
 
-@app.get("/hello")
-def hello():
-    return("hello world")
+@app.get("/")
+def root():
+    return {"message": "Hello akshay"}
+
+@app.get("/posts")
+def get_post():
+    return {"data": "your data"}
+
+@app.post("/create")
+def create_post(data: Post):
+    print(data)
+    return {f"First Name" : f"{data.firstName}" , 
+            f"Last Name" : f"{data.lastName}", 
+            f"Age" : f"{data.age}", 
+            f"Is Married" : f"{data.married}"
+        }
--- fastapi/pydantic.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/pydantic")
-def pydatntic():
-    return(this )
